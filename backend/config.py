2ff7f3493198794ab5f432e3b7c59160443987fb
imzhulei <7656577@qq.com>
2025-11-27 12:39:23 +0800

    init full backend and deploy config for distributed snmp network visualization system

--- backend/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI', 'mysql+pymysql://user:password@mysql/network_vis')
-CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'redis://redis:6379/0')
-CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND', 'redis://redis:6379/0')
